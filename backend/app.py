8ecd1c8a7ddf87174c05ad1b17df52fb7895e3ce
Nunie123 <ed.a.nunes@gmail.com>
2018-10-30 23:54:26 -0400

    Minor changes to get_parcel_summary

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ def format_address_direction(address):
 
 def get_property_assessment_by_address(address, zip_code):
     app_token = config.get('richmond_open_data', 'app_token')
-    url = 'https://data.richmondgov.com/resource/jde6-giuc.json?%24%24app_token=a3P1TkGmqcICo27FNEJlNI3yq'
+    url = 'https://data.richmondgov.com/resource/jde6-giuc.json?%24%24app_token={}'.format(app_token)
     url +="&$where=lower(address)='{}'&zip_code={}".format(address.lower(), zip_code)
     
     r = requests.get(url)
@@ -56,12 +56,19 @@ def get_property_assessment_by_address(address, zip_code):
             raise Exception('Bad Response: Returned status code is {}. Content: {}'.format(r.status_code, r.content))
         data = r.json()
         if not data:
-            formatted_address = format_address_direction(format_address_street_type(address))
+            formatted_address = format_address_direction(address)
             url_format_2 = url.replace(address, formatted_address)
             r = requests.get(url_format_2)
             if r.status_code != 200:
                 raise Exception('Bad Response: Returned status code is {}. Content: {}'.format(r.status_code, r.content))
             data = r.json()
+            if not data:
+                formatted_address = format_address_direction(format_address_street_type(address))
+                url_format_3 = url.replace(address, formatted_address)
+                r = requests.get(url_format_3)
+                if r.status_code != 200:
+                    raise Exception('Bad Response: Returned status code is {}. Content: {}'.format(r.status_code, r.content))
+                data = r.json()
     return data
 
 
@@ -74,7 +81,7 @@ def get_parcel_summary_by_address(address, zip_code):
     parcel_id = assessment_record[0]['pin']
 
     app_token = config.get('richmond_open_data', 'app_token')
-    url = 'https://data.richmondgov.com/resource/hi27-ghss.json?%24%24app_token=a3P1TkGmqcICo27FNEJlNI3yq'
+    url = 'https://data.richmondgov.com/resource/hi27-ghss.json?%24%24app_token={}'.format(app_token)
     url +="&$where=pin_pin='{}'".format(parcel_id)
     r = requests.get(url)
     if r.status_code != 200:
@@ -83,13 +90,11 @@ def get_parcel_summary_by_address(address, zip_code):
 
     if len(data) > 1:
         print('Warning: Associated Parcel ID Number (PIN) identified multiple parcel summary records.')
-
     return data[0]
 
 
 @app.route('/', methods=['GET','POST'])
 def provide_parcel_summary():
-    print(request.form)
     address = request.args.get('address')
     zip_code = request.args.get('zip_code')
     data = get_parcel_summary_by_address(address, zip_code)
